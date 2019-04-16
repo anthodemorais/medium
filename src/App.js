651d8011d5c23e7200660adb66b5915129a1a1c0
Anthony <anthonyd-m@live.com>
2019-04-16 12:28:32 +0200

    blog finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import User from './Components/User';
+import Article from './Components/Article';
+import AddArticle from './Components/AddArticle';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet"></link>
+        <Router>
+          <Route exact path={`/`} component={Home}></Route>
+          <Route exact path={`/login`} component={Login}></Route>
+          <Route exact path={`/register`} component={Register}></Route>
+          <Route exact path={`/users/me`} component={User}></Route>
+          <Route exact path={`/articles/:id`} component={Article}></Route>
+          <Route exact path={`/new/articles`} component={AddArticle}></Route>
+        </Router>
       </div>
     );
   }
